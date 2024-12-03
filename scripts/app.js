8c5e35d5863780ef9912c88cb01dc5c12d4cbcab
Kamyar <kamyarhmn@gmail.com>
2024-12-04 00:29:35 +0330

    fix: change zone of nav icon click

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@
 
 const navIcon = document.querySelector('.nav__toggle-icon');
 
-document.addEventListener('click', function () {
+navIcon.addEventListener('click', function () {
 	navIcon.classList.toggle('nav__toggle-line--clicked');
 	setTimeout(function () {
 		navIcon.classList.toggle('nav__toggle-line--clicked');
