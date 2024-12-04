32813d315c026375b23de08c42df7b4b091b0dc0
Kamyar <kamyarhmn@gmail.com>
2024-12-04 17:41:09 +0330

    feat: add hamburger mobile menu

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,25 @@
 'use strict';
 
 const navIcon = document.querySelector('.nav__toggle-icon');
+const mobileMenu = document.querySelector('.mobile-wrapper');
+const closeIcon = document.querySelector('.nav__close-icon');
+let isMenuOpen = false;
 
 navIcon.addEventListener('click', function () {
 	navIcon.classList.toggle('nav__toggle-line--clicked');
 	setTimeout(function () {
 		navIcon.classList.toggle('nav__toggle-line--clicked');
 	}, 150);
+	if (!isMenuOpen) {
+		mobileMenu.style.left = '0';
+		isMenuOpen = true;
+	} else {
+		mobileMenu.style.left = '-25rem';
+		isMenuOpen = false;
+	}
+});
+
+closeIcon.addEventListener('click', function () {
+	mobileMenu.style.left = '-25rem';
+	isMenuOpen = false;
 });
