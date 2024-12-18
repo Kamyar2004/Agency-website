1e70be9f17d9f22e3bf77f38553c45fbd36e61b6
Kamyar <kamyarhmn@gmail.com>
2024-12-18 19:59:05 +0330

    feat: add open and close feature to the question-body items

--- scripts/app.js
+++ scripts/app.js
@@ -9,6 +9,10 @@ const counter_2 = document.querySelector('.counter-2');
 const counter_3 = document.querySelector('.counter-3');
 const counter_4 = document.querySelector('.counter-4');
 const counter_5 = document.querySelector('.counter-5');
+const questionItems = document.querySelectorAll('.question-body__item');
+// const questionTitles = document.querySelectorAll('.question-body__title');
+// const showBtns = document.querySelectorAll('.show-icon');
+// const showBtnLines = document.querySelectorAll('.show-icon__line');
 
 navIcon.addEventListener('click', function () {
 	navIcon.classList.toggle('nav__toggle-line--clicked');
@@ -49,3 +53,28 @@ function animateNumber(element, start, end, duration) {
 // 	animateNumber(counter_4, 0, 420, 4000);
 // 	animateNumber(counter_5, 0, 10, 4000);
 // });
+
+questionItems.forEach(function (item) {
+	const showBtn = item.querySelector('.show-icon');
+	const questionTitle = item.querySelector('.question-body__title');
+	const showBtnLine = item.querySelector('.show-icon__line');
+
+	showBtn.addEventListener('click', function () {
+		if (item.classList.contains('question-body__item--open')) {
+			item.classList.remove('question-body__item--open');
+			questionTitle.classList.remove('question-body__title--open');
+			showBtnLine.classList.remove('show-icon__line--open');
+		} else {
+			questionItems.forEach(function (item) {
+				const questionTitle = item.querySelector('.question-body__title');
+				const showBtnLine = item.querySelector('.show-icon__line');
+				item.classList.remove('question-body__item--open');
+				questionTitle.classList.remove('question-body__title--open');
+				showBtnLine.classList.remove('show-icon__line--open');
+			});
+			item.classList.add('question-body__item--open');
+			questionTitle.classList.add('question-body__title--open');
+			showBtnLine.classList.add('show-icon__line--open');
+		}
+	});
+});
